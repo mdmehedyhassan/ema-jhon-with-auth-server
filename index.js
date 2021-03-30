b74bf645f92813214adc400cd52771720b4c3594
mdmehedyhassan <mdmehedyh55@gmail.com>
2021-03-30 16:19:40 +0600

    routh add

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(cors());
 
 const port = 5000;
 
+app.get('/', (req, res) => {
+  res.send("hello from db it's working ")
+})
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("emaJohnAuthStore").collection("products");
